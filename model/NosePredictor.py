536c08845629fa088c90c24b71e7ac4baef2265f
imBecky <hbinqian@gmail.com>
2024-11-02 23:46:08 +0800

    cos annealing regression

--- model/NosePredictor.py
+++ model/NosePredictor.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import numpy as np
 import torch.nn.functional as F
 import copy
 from utils.util import EMA
@@ -38,5 +39,26 @@ class GaussianDiffusion(nn.Module):
         sqrt_alpha_prod = self.sqrt_alpha_prod[:t]
         sqrt_one_minus_alpha_prod = (1.0 - self.alphas_cumprod[:t]) ** 0.5
         X_t = sqrt_alpha_prod * X0 + sqrt_one_minus_alpha_prod * noise
+        return X_t
 
 
+def generate_cosine_schedule(T, s=0.008):
+    def f(t, T):
+        return (np.cos((t / T + s) / (1 + s) * np.pi / 2)) ** 2
+
+    alphas = []
+    f0 = f(0, T)
+
+    for t in range(T + 1):
+        alphas.append(f(t, T) / f0)
+
+    betas = []
+
+    for t in range(1, T + 1):
+        betas.append(min(1 - alphas[t] / alphas[t - 1], 0.999))
+
+    return np.array(betas)
+
+
+def generate_linear_schedule(T, low, high):
+    return np.linspace(low, high, T)
