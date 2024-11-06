c6615a9e22d967b0c716f71a82e3c0883fff71b8
imBecky <hbinqian@gmail.com>
2024-11-06 23:50:29 +0800

    denoise sampling

--- model/NoisePredictor.py
+++ model/NoisePredictor.py
@@ -85,3 +85,47 @@ def q_sample(x_start, t, noise):
     x_t = sqrt_alphas_cumprod_t * x_start + sqrt_one_minus_alphas_cumprod_t * noise
     x_t = x_t.float()
     return x_t
+
+
+@torch.inference_mode()
+def p_sample(x, t, t_index, betas, sqrt_one_minus_alphas_cumprod, sqrt_recip_alphas, denoise_model, posterior_variance):
+    betas_t = extract(betas, t, x.shape)
+    sqrt_one_minus_alphas_cumprod_t = extract(sqrt_one_minus_alphas_cumprod, t, x.shape)
+    sqrt_recip_alphas_t = extract(sqrt_recip_alphas, t, x.shape)
+
+    # Equation 11 in the paper
+    # Use our model (noise predictor) to predict the mean
+    model_mean = sqrt_recip_alphas_t * (
+            x - betas_t * denoise_model(x, t) / sqrt_one_minus_alphas_cumprod_t
+    )
+
+    if t_index == 0:
+        return model_mean
+    else:
+        posterior_variance_t = extract(posterior_variance, t, x.shape)
+        noise = torch.randn_like(x)
+        # Algorithm 2 line 4:
+        return model_mean + torch.sqrt(posterior_variance_t) * noise
+
+
+@torch.inference_mode()
+def p_sample_loop(shape, betas, sqrt_one_minus_alphas_cumprod, sqrt_recip_alphas, denoise_model, posterior_variance, timesteps):
+    device = next(denoise_model.parameters()).device
+
+    b = shape[0]
+    # start from pure noise (for each example in the batch)
+    img = torch.randn(shape, device=device)
+    imgs = []
+
+    for i in tqdm(reversed(range(0, timesteps)), desc='sampling loop time step', total=timesteps):
+        img = p_sample(img, torch.full((b,), i, device=device, dtype=torch.long), i, betas, sqrt_one_minus_alphas_cumprod, sqrt_recip_alphas, denoise_model, posterior_variance)
+        imgs.append(img.cpu().numpy())
+    return imgs
+
+
+@torch.inference_mode()
+def sample(image_size, batch_size, channels, beta_array, sqrt_one_minus_alphas_cumprod,
+           sqrt_recip_alphas, denoise_model, posterior_variance, t):
+    return p_sample_loop(beta_array, sqrt_one_minus_alphas_cumprod, sqrt_recip_alphas, denoise_model,
+                         posterior_variance, t, shape=(batch_size, channels, image_size, image_size))
+
